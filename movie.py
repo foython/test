7734624d407f2e320a2a12916bb77ed38254e03a
foython <foython@gmail.com>
2025-07-23 17:05:57 +0600

    video_clip added

--- movie.py
+++ movie.py
@@ -1,4 +1,4 @@
-from moviepy import VideoFileClip, concatenate_videoclips, AudioFileClip
+from moviepy import VideoFileClip, AudioFileClip, concatenate_videoclips
 
 video_clip = VideoFileClip("output0.mp4")
 audio_clip = AudioFileClip("conversation.wav")
@@ -6,14 +6,22 @@ audio_clip = AudioFileClip("conversation.wav")
 video_clip_duration = video_clip.duration
 audio_clip_duration = audio_clip.duration
 
-new_video = [video_clip] 
-if audio_clip_duration > video_clip_duration:
+new_video_parts = [video_clip]
 
-    length = audio_clip_duration - video_clip_duration
-    subclip = video_clip.subclipped(0, length)
-    new_video.append(subclip)
-    final_clip = concatenate_videoclips(subclip)
-    
+if audio_clip_duration > video_clip_duration:
+    # Extend video by looping or repeating part
+    length_to_add = audio_clip_duration - video_clip_duration
+    subclip = video_clip.subclipped(0, min(length_to_add, video_clip.duration))
+    new_video_parts.append(subclip)
+
+# Concatenate all video parts
+final_video = concatenate_videoclips(new_video_parts)
+
+# Set the longer audio
+final_video = final_video.with_audio(audio_clip)
+
+# Write the output
+final_video.write_videofile("final_output.mp4", codec="libx264", audio_codec="aac")
 
 
 
